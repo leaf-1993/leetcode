f24c1cf39d568765293a34d7552fb5f78528e377
chenxiang <cx344794@alibaba-inc.com>
2021-12-12 10:03:26 +0800

    动态数组

--- src/main/java/com/cx/ds/ArrayList.java
+++ src/main/java/com/cx/ds/ArrayList.java
@@ -1,8 +1,10 @@
 package com.cx.ds;
 
+import java.util.List;
+
 /**
  * @author chenxiang
  * @create 2021-12-02 22:51
  */
-public class ArrayList {
+public class ArrayList<E> implements List<E> {
 }
